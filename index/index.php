1f6526b0ff646f1cc5e1768f5f8ed95653ecf2b6
Ainazik <92912243+Ainazil@users.noreply.github.com>
2021-10-27 13:04:01 +0600

    Update index.php

--- index/index.php
+++ index/index.php
@@ -5,3 +5,14 @@
 this is Ainazik's first web page
 </body>
 </html>
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>HTML Image</h2>
+<img src="pic_trulli.jpg" alt="Trulli" width="500" height="333">
+
+</body>
+</html>
