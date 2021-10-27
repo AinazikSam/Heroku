12ffb7c5b34a22c22b3fa1f1ef2322d0f1c7f310
Ainazik <92912243+Ainazil@users.noreply.github.com>
2021-10-27 14:12:46 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
 
+<h2>Halloween</h2>
+<img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.history.com%2F.image%2Ft_share%2FMTY3NDg4OTU3NjQxMTQwMDEz%2Fhalloween-gettyimages-172988453.jpg&imgrefurl=https%3A%2F%2Fwww.history.com%2Ftopics%2Fhalloween%2Fhistory-of-halloween&tbnid=mLjF1mgX54vT8M&vet=12ahUKEwi33tyjkurzAhXQyyoKHZQNAkUQMygBegUIARC0AQ..i&docid=VxRdJUxdcEQ3KM&w=1200&h=675&q=halloween&client=ubuntu&ved=2ahUKEwi33tyjkurzAhXQyyoKHZQNAkUQMygBegUIARC0AQ" alt="Hallows' Eve" width="500" height="600">
+
+</body>
+</html>
