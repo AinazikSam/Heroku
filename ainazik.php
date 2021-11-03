08f9419282c83c15c0eafcf5d0e9cf656b95c1ee
Ainazik <92912243+Ainazil@users.noreply.github.com>
2021-11-03 01:05:17 -0700

    Update ainazik.php

--- ainazik.php
+++ ainazik.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<html>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/3/w3.css">
 <body>
-    
+
+<!-- Content will go here -->
+
 </body>
 </html>
