db18dc9ad8cf7ae4981a69bb84c69b320fd33c1d
Ainazik <92912243+Ainazil@users.noreply.github.com>
2021-11-09 18:34:30 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,28 @@
-<form action="action_page.php" method="post">
-  <div class="imgcontainer">
-    <img src="img_avatar2.png" alt="Avatar" class="avatar">
-  </div>
+<html>
+<head>
 
-  <div class="container">
-    <label for="uname"><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="uname" required>
+<title>Login paget</title>
+</head>
+<script type="text/javascript">
+function display(form){
+if (form.username.value=="Ainazik") { 
+  if (form.password.value=="1234") {              
+          location="ainazik.php" 
+           } else {
+             alert("Invalid Password")
+             }
+        } else {  alert("Invalid Username")
+          }
+      }
+   </script>   
+<body >
 
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
+<form >
+<input type="text" name="username" /><br><br>
+<input type="password" name="password"/><br><br>
+<input type="button" value="Login" onClick="display(this.form)"/>
 
-    <button type="submit">Login</button>
-    <label>
-      <input type="checkbox" checked="checked" name="remember"> Remember me
-    </label>
-  </div>
-
-  <a href="ainazik.php">link Ainazik</a>
-  
-  <div class="container" style="background-color:#f1f1f1">
-    <button type="button" class="cancelbtn">Cancel</button>
-    <span class="psw">Forgot <a href="#">password?</a></span>
-  </div>
 </form>
+
+</body>
+</html>
