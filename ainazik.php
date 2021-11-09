bf6b9162053be787526aa038fe5664003ac5d5c6
Ainazik <92912243+Ainazil@users.noreply.github.com>
2021-11-09 19:19:22 +0600

    Update ainazik.php

--- ainazik.php
+++ ainazik.php
@@ -34,7 +34,7 @@
 
 						<td>
 							<a href="#home" style="text-decoration:none">
-								<font face="Verdana" size="5" color=black>
+								<font face="Verdana" size="5" color=darkblue>
 									Home
 								</font>
 							</a>
@@ -46,7 +46,7 @@
 
 						<td>
 							<a href="#about" style="text-decoration:none">
-								<font face="Verdana" size="5" color=black>
+								<font face="Verdana" size="5" color=darkblue>
 									About
 								</font>
 							</a>
@@ -58,7 +58,7 @@
 
 						<td>
 							<a href="#projects" style="text-decoration:none">
-								<font face="Verdana" size="5" color=black>
+								<font face="Verdana" size="5" color=darkblue>
 									Projects
 								</font>
 							</a>
@@ -66,23 +66,12 @@
 
 						<td>
 							|
-						</td>
-
-						<td>
-							<a href="#achievements" style="text-decoration:none">
-								<font face="Verdana" size="5" color=black>
-									Achievements
-								</font>
-							</a>
-						</td>
-
-						<td>
-							|
+						
 						</td>
 
 						<td>
 							<a href="#contact" style="text-decoration:none">
-								<font face="Verdana" size="5" color=black>
+								<font face="Verdana" size="5" color=darkblue>
 									Contact
 								</font>
 							</a>
@@ -96,7 +85,7 @@
 
 	<!--Home(start)-->
 	<table id="home" border="1" width="100%"
-		cellpadding="20" cellspacing="0" bgcolor="black">
+		cellpadding="20" cellspacing="0" bgcolor="blue">
 		<tr>
 			<td>
 				<table border="0" cellpadding="15"
@@ -151,40 +140,18 @@
 						<td width="60%">
 							<font face="Verdana" size="4"
 								color="white">
-								Thanks for your interest, here
-								is a quick story of me and this
-								website.
+								
 								<hr color="black">
-								Sandeep Jain An IIT Roorkee alumnus
-								and founder of GeeksforGeeks. He
-								loves to solve programming problems
-								in most efficient ways. Apart from
-								GeeksforGeeks, he has worked with
-								DE Shaw and Co. as a software
-								developer and JIIT Noida as an
-								assistant professor.
+								
 								<hr color="black">
 
-								I do my work mainly in C-Language,
-								C++ and JAVA. C++ and Data Structure
-								& Algorithm are my stronger section.
-								Besides these I know Web Development,
-								LINUX and database as well.
+								
 								<hr color="black">
 
-								This website is basically one of my
-								Web Development project which is
-								built using HTML only.
-								Here one can also find ideas for
-								projects in different languages.
-
-								Thanks again for reading this,
-								because of people like you, it
-								exists and prospers!
 								<hr color="black">
-								Cheers,
+								
 								<br>
-								<b>GeeksForGeeks.</b>
+								<b></b>
 							</font>
 						</td>
 					</tr>
@@ -348,89 +315,6 @@
 		</tr>
 	</table>
 	<!--Projects(end)-->
-
-	<!--Achievement(start)-->
-	<table id="achievements" border="0" width="100%"
-		cellpadding="0" cellspacing="0" bgcolor="black">
-		<tr>
-			<td>
-				<table border="0" cellpadding="15"
-					cellspacing="0" width="80%" align="center">
-					<tr>
-						<td height="180" align="center" valign="middle">
-							<font face="Verdana" size="7" color="#4CAF50">
-								Achievements
-								<hr color="#4CAF50" width="100">
-							</font>
-						</td>
-					</tr>
-
-					<tr>
-						<td>
-							<font face="Verdana" size="4" color="white">
-								<ul>
-									<li>
-										<b>Intern at GeeksforGeeks.</b>
-										<ul>
-											<li>
-												December,2020 - Present.
-											</li>
-											<li>
-												Write technical articles
-												on programming related topics.
-											</li>
-										</ul>
-									</li>
-
-									<li>
-										<hr color="black">
-										<hr color="black">
-										<hr color="black">
-										<b>Microsoft Learn Student Ambassador.</b>
-										<ul>
-											<li>
-												August,2020 - Present.
-											</li>
-											<li>
-												Conducted events and workshops on
-												different technologies.
-											</li>
-										</ul>
-									</li>
-
-									<li>
-										<hr color="black">
-										<hr color="black">
-										<hr color="black">
-										<b>Mentored HackTX.</b>
-										<ul>
-											<li>
-												October,2020.
-											</li>
-											<li>
-												Selected from Microsoft as Student
-												ambassador where I mentored students
-												in their projects.
-											</li>
-											<hr color="black">
-											<hr color="black">
-											<hr color="black">
-											<hr color="black">
-											<hr color="black">
-										</ul>
-									</li>
-
-								</ul>
-
-							</font>
-						</td>
-					</tr>
-				</table>
-			</td>
-		</tr>
-	</table>
-	<!--Achievement(end)-->
-
 
 	<!--Contact(start)-->
 	<table id="contact" border="0" width="100%"
@@ -655,7 +539,7 @@
 						<td width="80%" valign="top">
 							<font face="Verdana"
 								color="#4CAF50" size="5">
-								©Copyright 2050 by nobody.
+								©Copyright 2022 by nobody.
 								All rights reserved.
 							</font>
 						</td>
