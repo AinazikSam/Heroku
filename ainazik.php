2bb6f7c1587b987d0aca2659bdb27b0e666f723f
Ainazik <92912243+Ainazil@users.noreply.github.com>
2021-11-12 15:23:03 +0600

    Update ainazik.php

--- ainazik.php
+++ ainazik.php
@@ -2,34 +2,17 @@
   <!-- https://www.pexels.com/video/plexus-of-abstract-geometrical-lines-and-figures-3129576/ --> 
   <section class="showcase"> 
     <header> 
-      <h2 class="logo">Travel</h2> 
+      <h2 class="logo">Coder</h2> 
       <div class="toggle"></div> 
       <link rel=¨stylesheet¨ type=¨text/css¨ href=¨..\SG\ai.css¨>
     </header> 
     <video src="https://traversymedia.com/downloads/videos/explore.mp4" muted loop autoplay></video> 
     <div class="overlay"></div> 
     <div class="text"> 
-      <h2>Never Stop To </h2>  
-      <h3>Exploring The World</h3> 
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod 
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-      consequat.</p> 
+      <h2>Portfolio</h2>  
+      <h3>About me</h3> 
+      <p>Something is here</p> 
       <a href="#">Explore</a> 
       <script src="..\SG\ai.js"></script>
     </div> 
-    <ul class="social"> 
-      <li><a href="#"><img src="https://i.ibb.co/x7P24fL/facebook.png"></a></li> 
-      <li><a href="#"><img src="https://i.ibb.co/Wnxq2Nq/twitter.png"></a></li> 
-      <li><a href="#"><img src="https://i.ibb.co/ySwtH4B/instagram.png"></a></li> 
-    </ul> 
-  </section> 
-  <div class="menu"> 
-    <ul> 
-      <li><a href="#">Home</a></li> 
-      <li><a href="#">News</a></li> 
-      <li><a href="#">Destination</a></li> 
-      <li><a href="#">Blog</a></li> 
-      <li><a href="#">Contact</a></li> 
-    </ul> 
-  </div> 
+    </div> 
