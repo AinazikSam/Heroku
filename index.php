0554a3ecee091588bc5cd7c3b6c05fbed62246b5
Ainazik <92912243+Ainazil@users.noreply.github.com>
2021-11-12 16:58:27 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,36 @@
 <html>
+
 <head>
 
-<title>Login paget</title>
+    <title>Login page</title>
+    <link rel="stylesheet" href="login.css">
 </head>
 <script type="text/javascript">
-function display(form){
-if (form.username.value=="Ainazik") { 
-  if (form.password.value=="1234") {              
-          location="ainazik.php" 
-           } else {
-             alert("Invalid Password")
-             }
-        } else {  alert("Invalid Username")
-          }
-      }
-   </script>   
-<body >
+    function display(form) {
+        if (form.username.value == "Demo") {
+            if (form.password.value == "12345") {
+                location = "ainazik.php"
+            } else {
+                alert("Invalid Password")
+            }
+        } else {
+            alert("Invalid Username")
+        }
+    }
+</script>
 
-<form >
-<input type="text" name="username" /><br><br>
-<input type="password" name="password"/><br><br>
-<input type="button" value="Login" onClick="display(this.form)"/>
+<body>
+    <div class="login-box">
+        <h1>Login</h1>
+    </div>
+    <form>
+        <input type="text" placeholder="Username" name="username" /><br><br>
+        <input type="password" placeholder="Password" name="password" /><br><br>
+        <input type="button" value="Sign in" onClick="display(this.form)" />
 
-</form>
+    </form>
 
 </body>
+
+</html>
 </html>
