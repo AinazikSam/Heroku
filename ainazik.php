6eac3faede55ca9593a3e5d5fdedae1976304249
Ainazik <92912243+Ainazil@users.noreply.github.com>
2021-11-16 21:05:14 +0600

    Update ainazik.php

--- ainazik.php
+++ ainazik.php
@@ -1,15 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="ai.css">
     <title>Ainazik</title>
-    
 </head>
-
 <body>
     <section class="showcase">
         <header>
@@ -27,6 +24,10 @@
             <p>Surname: Samatova </p>
             <p>Phone number : +996 705 313 414</p>
             <p>Address: Bishkek Ala-To2 st.Ulul-To 59t</P>
+        </div>
+        </div>
+        <div class="photo">
+            <img src="avatar-girl.png" alt="">
         </div>
      <ul class="social">
             <li><a href="https://www.facebook.com/"><img src="https://i.ibb.co/x7P24fL/facebook.png"></a></li>
